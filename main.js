9c71b3463cf73a58e3b66b84d1b467c33eec5a2c
abduvohidovnurmuhammad <abduvohidovn7@gmail.com>
2022-11-24 02:08:48 +0500

    arrey

--- main.js
+++ main.js
@@ -10,20 +10,30 @@
     // console.log(faktaryal(prompt("son kiriting faktaryal aniqlaymiz")));
     // ---------------object-------------------------------------------
 
-        let savol = prompt("moshina malumotini biuz bilan bolishasizmi ha ni kiriting ")
-        if( savol=="ha"){
-          alert("boshladik")
-        } 
+        // let savol = prompt("moshina malumotini biuz bilan bolishasizmi ha ni kiriting ")
+        // if( savol=="ha"){
+        //   alert("boshladik")
+        // } 
 
-        const obj ={
-           nomi : prompt("yoqtirgan moshinangizni nomini kiriting"),
-            rang : prompt("rangini kiriting"),
-            yil : prompt("yilini kiriting"),
-            davlat : prompt("qaysi davlatdan ishlab chiqariladi")
+        // const obj ={
+        //    nomi : prompt("yoqtirgan moshinangizni nomini kiriting"),
+        //     rang : prompt("rangini kiriting"),
+        //     yil : prompt("yilini kiriting"),
+        //     davlat : prompt("qaysi davlatdan ishlab chiqariladi")
 
-        }
+        // }
 
-            console.log(obj);
+        //     console.log(obj);
+            let arr = [ 1,2,3,4,5,6,5,6,3,2,4,3,1];
+            function x (arr){
+                let obj = {}
+                for( let i = 0 ; i <arr.length; i++){
+                    obj[arr[i]]=arr[i]
+                }
+                return Object.values(obj)
+            }
+            console.log(x(arr));
+
 
 
         
