2a312d0597a49572cf2824e45aebe1bedeb6e586
abduvohidovnurmuhammad <abduvohidovn7@gmail.com>
2022-11-24 02:19:44 +0500

    arrey3

--- main.js
+++ main.js
@@ -24,19 +24,36 @@
         // }
 
         //     console.log(obj);
-            let arr = [ 1,2,3,4,5,6,5,6,3,2,4,3,1];
-            function x (arr){
-                let obj = {}
-                for( let i = 0 ; i <arr.length; i++){
-                    obj[arr[i]]=arr[i]
+            // let arr = [ 1,2,3,4,5,6,5,6,3,2,4,3,1];
+            // function x (arr){
+            //     let obj = {}
+            //     for( let i = 0 ; i <arr.length; i++){
+            //         obj[arr[i]]=arr[i]
+            //     }
+            //     return Object.values(obj)
+            // }
+            // console.log(x(arr));
+
+
+
+        // let num = [1,2,3,4,5,6,7,8,9,10]
+        //     for (let i = 0; i <num.length; i++) {
+        //         if( num[i]%2==0)
+        //         console.log(num[i]);
+                
+        //     }
+        let arrey = [1,3,5,66,88,76,545]
+        function x(arrey){
+            let num = 0
+            for( let i = 0 ; i <arrey.length; i ++){
+                if( arrey[i]>num){
+                    num= arrey[i]
                 }
-                return Object.values(obj)
             }
-            console.log(x(arr));
+            return num
+        }
+        console.log(x(arrey));
 
-
-
-        
 
         
 
