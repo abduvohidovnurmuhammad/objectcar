8dce0f0640cfb2479b4f9368f8327cef9adf152b
abduvohidovnurmuhammad <abduvohidovn7@gmail.com>
2022-11-23 23:41:58 +0500

    object

---
+++ main.js
@@ -0,0 +1,34 @@
+        
+    // function faktaryal(a) {
+    //     if (a===0) {
+    //         return 1
+    //     }
+    //     else{
+    //         return a*faktaryal(a-1)
+    //     }
+    // }
+    // console.log(faktaryal(prompt("son kiriting faktaryal aniqlaymiz")));
+    // ---------------object-------------------------------------------
+
+        let savol = prompt("moshina malumotini biuz bilan bolishasizmi ha ni kiriting ")
+        if( savol=="ha"){
+          alert("boshladik")
+        } 
+
+        const obj ={
+           nomi : prompt("yoqtirgan moshinangizni nomini kiriting"),
+            rang : prompt("rangini kiriting"),
+            yil : prompt("yilini kiriting"),
+            davlat : prompt("qaysi davlatdan ishlab chiqariladi")
+
+        }
+
+            console.log(obj);
+
+
+        
+
+        
+
+
+    
